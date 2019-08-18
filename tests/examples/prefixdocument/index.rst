8faa8c5f6a84c4789ee5bf793ecc88dd5c6211f4
Manuel Kaufmann <humitos@gmail.com>
2019-08-18 20:22:23 +0200

    More test cases

---
+++ tests/examples/prefixdocument/index.rst
@@ -0,0 +1,7 @@
+Example page
+============
+
+This is an example page.
+
+:ref:`This a :ref: to Chapter I <chapter-i:Chapter I>`.
+:hoverxref:`This a :hoverxref: to Chapter I <chapter-i:Chapter I>`.
--- tests/test_htmltag.py
+++ tests/test_htmltag.py
@@ -1,5 +1,6 @@
 import os
 import pytest
+import shutil
 
 
 srcdir = os.path.join(
@@ -8,22 +9,36 @@ srcdir = os.path.join(
     'default',
 )
 
+# srcdir with ``autosectionlabel_prefix_document = True`` config
+prefixdocumentsrcdir = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)),
+    'examples',
+    'prefixdocument',
+)
+
+
+@pytest.fixture(autouse=True, scope='function')
+def remove_sphinx_build_output():
+    """Remove _build/ folder, if exist."""
+    for path in (srcdir, prefixdocumentsrcdir):
+        build_path = os.path.join(path, '_build')
+        if os.path.exists(build_path):
+            shutil.rmtree(build_path)
+
 
 @pytest.mark.sphinx(
     srcdir=srcdir,
-    confoverrides={
-        'autosectionlabel_prefix_document': True,
-        'hoverxref_auto_ref': True,
-    },
 )
 def test_default_settings(app, status, warning):
+    """The extension should not change the output if not configured."""
     app.build()
     path = app.outdir / 'index.html'
-    assert path.exists() == True
+    assert path.exists() is True
     content = open(path).read()
 
     chunks = [
-        '<a class="reference internal" href="chapter-i.html#chapter-i"><span class="std std-ref">This a reference to Chapter I</span></a>',
+        '<a class="reference internal" href="chapter-i.html#chapter-i"><span class="std std-ref">This a :ref: to Chapter I</span></a>',
+        '<a class="reference internal" href="chapter-i.html#section-i"><span class="std std-ref">This a :hoverxref: to Chapter I, Section I</span></a>',
     ]
 
     for chunk in chunks:
@@ -35,18 +50,39 @@ def test_default_settings(app, status, warning):
     confoverrides={
         'hoverxref_project': 'myproject',
         'hoverxref_version': 'myversion',
-        'autosectionlabel_prefix_document': True,
-        'hoverxref_auto_ref': True,
     },
 )
 def test_project_version_settings(app, status, warning):
     app.build()
     path = app.outdir / 'index.html'
-    assert path.exists() == True
+    assert path.exists() is True
     content = open(path).read()
 
     chunks = [
-        '<a class="hoverxref reference internal" data-doc="chapter-i" data-project="myproject" data-section="chapter-i" data-version="myversion" href="chapter-i.html#chapter-i"><span class="std std-ref">This a reference to Chapter I</span></a>',
+        '<a class="reference internal" href="chapter-i.html#chapter-i"><span class="std std-ref">This a :ref: to Chapter I</span></a>',
+        '<a class="hoverxref reference internal" data-doc="chapter-i" data-project="myproject" data-section="section-i" data-version="myversion" href="chapter-i.html#section-i"><span class="std std-ref">This a :hoverxref: to Chapter I, Section I</span></a>',
+    ]
+
+    for chunk in chunks:
+        assert chunk in content
+
+
+@pytest.mark.sphinx(
+    srcdir=prefixdocumentsrcdir,
+    confoverrides={
+        'hoverxref_project': 'myproject',
+        'hoverxref_version': 'myversion',
+    },
+)
+def test_autosectionlabel_project_version_settings(app, status, warning):
+    app.build()
+    path = app.outdir / 'index.html'
+    assert path.exists() is True
+    content = open(path).read()
+
+    chunks = [
+        '<a class="reference internal" href="chapter-i.html#chapter-i"><span class="std std-ref">This a :ref: to Chapter I</span></a>.',
+        '<a class="hoverxref reference internal" data-doc="chapter-i" data-project="myproject" data-section="chapter-i" data-version="myversion" href="chapter-i.html#chapter-i"><span class="std std-ref">This a :hoverxref: to Chapter I</span></a>',
     ]
 
     for chunk in chunks:
