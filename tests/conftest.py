3dd5eaf563960a6bbd613968186e0aa163c02cf3
Manuel Kaufmann <humitos@gmail.com>
2020-04-16 13:48:55 +0200

    Refactor tests to share functionability

---
+++ tests/conftest.py
@@ -0,0 +1,14 @@
+import os
+import shutil
+import pytest
+
+from .utils import srcdir, prefixdocumentsrcdir, customobjectsrcdir, pythondomainsrcdir
+
+
+@pytest.fixture(autouse=True, scope='function')
+def remove_sphinx_build_output():
+    """Remove _build/ folder, if exist."""
+    for path in (srcdir, prefixdocumentsrcdir, customobjectsrcdir, pythondomainsrcdir):
+        build_path = os.path.join(path, '_build')
+        if os.path.exists(build_path):
+            shutil.rmtree(build_path)
